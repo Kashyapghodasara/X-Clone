464921f0789ed5f90f5896de6c9780168db24718
Kashyap Ghodasara <kashyapghodasara007@gmail.com>
2024-12-24 16:52:38 +0530

    database.js

--- Back-End/config/database.js
+++ Back-End/config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const DBConnect = () => {
     mongoose.connect(process.env.MONGO_URI).then(()=>{
-        logger.info("MongoDB Connected Successfully ⚙");
+        logger.debug("MongoDB Connected Successfully ⚙");
     }).catch((error)=>{
         logger.critical("Connection Error", error.message);
     })
