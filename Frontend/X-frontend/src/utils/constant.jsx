899ce943f2defdddd698c51ac0af6bad328c7ef8
Kashyapghodasara <kashyapghodasara007@gmail.com>
2025-02-13 14:46:03 +0530

    deployement

--- Frontend/X-frontend/src/utils/constant.jsx
+++ Frontend/X-frontend/src/utils/constant.jsx
@@ -1,2 +1,2 @@
-export const USER_API_ENDPOINT = "http://localhost:8080/api/v1/user";
-export const TWEET_API_ENDPOINT = "http://localhost:8080/api/v1/tweet";
+export const USER_API_ENDPOINT = "https://x-clone-backend-c9aw.onrender.com/api/v1/user";
+export const TWEET_API_ENDPOINT = "https://x-clone-backend-c9aw.onrender.com/api/v1/tweet";
