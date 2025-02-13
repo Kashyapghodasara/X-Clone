46979aa7985bc2552c4f38d956873336a3ea0353
Kashyapghodasara <kashyapghodasara007@gmail.com>
2025-02-13 14:51:19 +0530

    deployement

--- Frontend/X-frontend/src/components/Body.jsx
+++ Frontend/X-frontend/src/components/Body.jsx
@@ -6,7 +6,7 @@ import Feed from './Feed'
 import Profile from './Profile'
 import Edit from './Edit'
 import Bookmark from './Bookmark'
-import UploadPhoto from './uploadPhoto'
+import UploadPhoto from './UploadPhoto'
 const Body = () => {
     const appRouter = createBrowserRouter([
         // This is an Array of Object
